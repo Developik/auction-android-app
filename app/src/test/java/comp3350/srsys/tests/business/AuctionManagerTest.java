7c39647a2c513422f62ab3f47e3ccd3e4d3720ad
brettaseltine <brettsky81@gmail.com>
2022-06-16 14:07:51 -0500

    Updated AuctionManager, BidTest, and AuctionManagerTest to account for Bid holding value of type double.

--- app/src/test/java/comp3350/srsys/tests/business/AuctionManagerTest.java
+++ app/src/test/java/comp3350/srsys/tests/business/AuctionManagerTest.java
@@ -13,24 +13,24 @@ public class AuctionManagerTest extends TestCase {
 
     public void setUp() {
         am = new AuctionManager(3);
-        u1 = new User("username", "john", "smith", "55 road dr.", 44);
-        u2 = new User("username", "bob", "last", "10 lol cres.", 2);
+        u1 = new User("username", "john", "smith", "55 road dr.", 44, false);
+        u2 = new User("username", "bob", "last", "10 lol cres.", 2, false);
     }
 
     public void testAddBid() {
         System.out.println("Running testAddBid");
         System.out.println("normal bids");
         setUp();
-        am.addBid(15, u1);
-        am.addBid(7, u2);
-        am.addBid(40, u1);
+        am.addBid(15.00, u1);
+        am.addBid(7.00, u2);
+        am.addBid(40.00, u1);
         assertEquals(3, am.getBidCount());
 
         System.out.println("negative bid should not be added");
         setUp();
-        am.addBid(15, u1);
-        am.addBid(7, u2);
-        am.addBid(-10, u1);
+        am.addBid(15.00, u1);
+        am.addBid(7.00, u2);
+        am.addBid(-10.00, u1);
         assertEquals(2, am.getBidCount());
 
         System.out.println("bid of value 0 should not be added");
@@ -57,6 +57,13 @@ public class AuctionManagerTest extends TestCase {
         am.addBid(15, u1);
         am.addBid(25, null);
         assertEquals(1, am.getBidCount());
+
+        System.out.println("bids with more than 2 decimal values should be added");
+        setUp();
+        am.addBid(15.00000, u1);
+        am.addBid(15.6789, u1);
+        assertEquals(2, am.getBidCount());
+
     }
 
     public void testGetBidCount() {
@@ -84,69 +91,86 @@ public class AuctionManagerTest extends TestCase {
 
         System.out.println("normal test");
         setUp();
-        am.addBid(6, u1);
-        am.addBid(11, u2);
-        am.addBid(5, u1);
+        am.addBid(6.50, u1);
+        am.addBid(11.00, u2);
+        am.addBid(5.01, u1);
         // wait for auction to end
         Thread.sleep(4000);
-        assertEquals(11, am.getWinner().getValue());
+        assertEquals(11.00, am.getWinner().getValue());
         assertEquals(u2, am.getWinner().getUser());
 
         System.out.println("auction with invalid bid");
         setUp();
-        am.addBid(6, u1);
-        am.addBid(-5, u1);
-        am.addBid(2, u2);
+        am.addBid(6.01, u1);
+        am.addBid(-5.00, u1);
+        am.addBid(2.00, u2);
         Thread.sleep(4000);
-        assertEquals(6, am.getWinner().getValue());
+        assertEquals(6.01, am.getWinner().getValue());
         assertEquals(u1, am.getWinner().getUser());
 
         System.out.println("calling before auction completion");
         setUp();
-        am.addBid(6, u1);
-        am.addBid(11, u2);
-        am.addBid(5, u1);
+        am.addBid(6.00, u1);
+        am.addBid(11.00, u2);
+        am.addBid(5.00, u1);
         assertNull(am.getWinner());
 
         System.out.println("duplicate values by same user");
         setUp();
-        am.addBid(6, u1);
-        am.addBid(4, u2);
-        am.addBid(6, u1);
+        am.addBid(6.00, u1);
+        am.addBid(4.00, u2);
+        am.addBid(6.00, u1);
         Thread.sleep(4000);
-        assertEquals(6, am.getWinner().getValue());
+        assertEquals(6.00, am.getWinner().getValue());
         assertEquals(u1, am.getWinner().getUser());
 
         System.out.println("duplicate values from different users should be won by earlier voter");
         setUp();
-        am.addBid(6, u1);
-        am.addBid(6, u2);
+        am.addBid(6.00, u1);
+        am.addBid(6.00, u2);
         Thread.sleep(4000);
-        assertEquals(6, am.getWinner().getValue());
+        assertEquals(6.00, am.getWinner().getValue());
         assertEquals(u1, am.getWinner().getUser());
 
         System.out.println("duplicate values from different users, but one user tops after");
         setUp();
-        am.addBid(6, u1);
-        am.addBid(6, u2);
-        am.addBid(7, u2);
+        am.addBid(6.00, u1);
+        am.addBid(6.00, u2);
+        am.addBid(7.00, u2);
         Thread.sleep(4000);
-        assertEquals(7, am.getWinner().getValue());
+        assertEquals(7.00, am.getWinner().getValue());
         assertEquals(u2, am.getWinner().getUser());
 
         System.out.println("adding highest bid after auction has closed (therefore this bid should not be added)");
         setUp();
-        am.addBid(10, u1);
-        am.addBid(20, u2);
+        am.addBid(10.00, u1);
+        am.addBid(20.00, u2);
         Thread.sleep(4000);
-        am.addBid(40, u1);
-        assertEquals(20, am.getWinner().getValue());
+        am.addBid(40.00, u1);
+        assertEquals(20.00, am.getWinner().getValue());
         assertEquals(u2, am.getWinner().getUser());
 
         System.out.println("no bids have been made, expect no winning bid");
         setUp();
         Thread.sleep(4000);
         assertNull(am.getWinner());
+
+        System.out.println("checking 1 cent different noticed");
+        setUp();
+        am.addBid(10.00, u1);
+        am.addBid(10.01, u2);
+        Thread.sleep(4000);
+        assertEquals(10.01, am.getWinner().getValue());
+        assertEquals(u2, am.getWinner().getUser());
+
+        System.out.println("numbers past 100's decimal pos will be dropped, not rounded");
+        setUp();
+        am.addBid(9.9999999, u1);
+        am.addBid(10.00, u2);
+        Thread.sleep(4000);
+        assertEquals(10.00, am.getWinner().getValue());
+        assertEquals(u2, am.getWinner().getUser());
+
     }
 
     public void testGetSecondsRemaining() throws InterruptedException {
